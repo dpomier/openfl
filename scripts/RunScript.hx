88757cb93e1ec396bff65609794938913caa97f0
dpomier <dpomier@spiele-palast.de>
2020-02-04 11:25:23 +0100

    add scoped commands

--- scripts/RunScript.hx
+++ scripts/RunScript.hx
@@ -35,6 +35,8 @@ class RunScript
 			Log.error("Cannot set current working directory to \"" + workingDirectory + "\"");
 		}
 
+		Haxelib.workingDirectory = workingDirectory;
+
 		if (args.length > 1 && args[0] == "create")
 		{
 			// args[1] = "openfl:" + args[1];
@@ -45,31 +47,82 @@ class RunScript
 
 			if (limeDirectory == null || limeDirectory == "" || limeDirectory.indexOf("is not installed") > -1)
 			{
-				Sys.command("haxelib", ["install", "lime"]);
+				Sys.command("haxelib install lime");
 			}
 		}
 		else if (args.length > 0 && args[0] == "process")
 		{
-			Sys.setCwd(cacheDirectory);
 
-			if (!FileSystem.exists("scripts/tools.n"))
-			{
-				rebuildTools();
-			}
+			var toolsPath = checkTools(workingDirectory);
 
-			Sys.exit(Sys.command("neko", ["scripts/tools.n"].concat(Sys.args())));
+			Sys.exit(Sys.command("neko", [toolsPath].concat(Sys.args())));
 			return;
 		}
+		else if (args.length > 1 && args[0] == "rebuild")
+		{
+			checkTools(workingDirectory, args[1] == "tools");
+		}
+		else if (args.length > 1 && args[0] == "scope")
+		{
+			switch args[1]
+			{
+				case "create":
+					System.makeDirectory(".openfl");
 
-		var args = ["run", "lime"].concat(args);
-		Sys.exit(Sys.command("haxelib", args.concat(["-openfl"])));
+				case "delete":
+					System.removeDirectory(".openfl");
+
+				default:
+					Log.error("Incorrect arguments for command 'scope'");
+					return;
+			}
+		}
+
+		Sys.exit(Sys.command("haxelib run lime " + args.join(" ") + " -openfl"));
 	}
 
-	private static function rebuildTools(rebuildBinaries = true):Void
+	private static function directoryExist(path:String):Bool
+	{
+		return FileSystem.exists(path) && FileSystem.isDirectory(path);
+	}
+
+	private static function hasTools(directory:String):Bool
+	{
+		return FileSystem.exists(directory + "/tools.n");
+	}
+
+	private static function checkTools(workingDirectory:String, rebuild:Bool = false):String
 	{
 		var openflDirectory = Haxelib.getPath(new Haxelib("openfl"), true);
 		var scriptsDirectory = Path.combine(openflDirectory, "scripts");
 
-		System.runCommand(scriptsDirectory, "haxe", ["tools.hxml"]);
+		if (directoryExist(workingDirectory + "/.openfl"))
+		{
+			if (rebuild || !hasTools(workingDirectory + "/.openfl"))
+			{
+				rebuildTools(scriptsDirectory, workingDirectory + "/.openfl");
+			}
+
+			return workingDirectory + "/.openfl/tools.n";
+		}
+		else
+		{
+			if (rebuild || !hasTools(scriptsDirectory))
+			{
+				rebuildTools(scriptsDirectory);
+			}
+
+			return scriptsDirectory + "/tools.n";
+		}
+	}
+
+	private static function rebuildTools(scriptsDirectory:String, ?targetDirectory:String, rebuildBinaries = true):Void
+	{
+		if (targetDirectory == null)
+		{
+			targetDirectory = scriptsDirectory;
+		}
+
+		HXML.buildFile(scriptsDirectory + "/tools.hxml", targetDirectory);
 	}
 }
